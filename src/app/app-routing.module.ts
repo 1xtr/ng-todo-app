761f9327e2429c8b56420a76254b742018b15399
Aleksey Anisimov <xtr@trendco.space>
2021-11-10 17:09:23 +0300

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {AuthComponent} from "./auth/auth.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'auth', component: AuthComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
