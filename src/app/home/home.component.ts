64b83faafa7a7f6105c93cdf2080e5c7aa8a9566
Aleksey Anisimov <xtr@trendco.space>
2021-11-15 12:27:43 +0300

    home component changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ILoginResponseData, UserService} from '../_services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  user: ILoginResponseData | {} | undefined
+  constructor(private appUserService: UserService) { }
 
   ngOnInit(): void {
+    this.user = this.appUserService.userData
   }
 
 }
