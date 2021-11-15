a31dd9e495d0c1b23b27de7008c260b4f709d5df
Aleksey Anisimov <xtr@trendco.space>
2021-11-16 00:57:06 +0300

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,4 +6,15 @@ export const environment: Environment = {
   LOGIN_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
   REG_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
   FORGOT_PASSWORD_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:createAuthUri?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
+  CHECK_EMAIL_EXIST_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:createAuthUri?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
+  REFRESH_TOKEN_URL: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
+  GET_USER_DATA_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
+  firebaseConfig: {
+    apiKey: "AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y",
+    authDomain: "test-project-67b13.firebaseapp.com",
+    projectId: "test-project-67b13",
+    storageBucket: "test-project-67b13.appspot.com",
+    messagingSenderId: "344370951376",
+    appId: "1:344370951376:web:6fb86c1deee5d761431ad5"
+  }
 };
--- src/environments/interface.ts
+++ src/environments/interface.ts
@@ -4,4 +4,15 @@ export interface Environment {
   REG_URL: string
   LOGIN_URL: string
   FORGOT_PASSWORD_URL: string
+  CHECK_EMAIL_EXIST_URL: string
+  REFRESH_TOKEN_URL: string
+  GET_USER_DATA_URL: string
+  firebaseConfig: {
+    apiKey: string
+    authDomain: string
+    projectId: string
+    storageBucket: string
+    messagingSenderId: string
+    appId: string
+  }
 }
