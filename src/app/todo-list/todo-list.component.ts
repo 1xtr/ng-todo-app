602f822d9dcc81b88fb02c97313aadc25a9ac420
Aleksey Anisimov <xtr@trendco.space>
2021-11-18 01:02:00 +0300

    change todo-list component. add table

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MatAccordion} from "@angular/material/expansion";
+import {ITodoList} from "../shared/Interfaces";
+import {TodoListService} from "../_services/todo-list.service";
+import {Subscription} from "rxjs";
 
-export interface TodoList {
-  id: string
-  title: string
-  owner_id: string
-  create_date: string
-  last_modify?: { user_id: string, date: string }
-  share?: {
-    url: string,
-    access_type: string
-  }[]
+export interface PeriodicElement {
+  name: string;
 }
 
 @Component({
@@ -17,12 +13,41 @@ export interface TodoList {
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
 
-  constructor() {
+export class TodoListComponent implements OnInit, OnDestroy {
+  @ViewChild(MatAccordion) accordion: MatAccordion | undefined
+  allSubs: Subscription | undefined
+
+  TODO_LISTS: ITodoList[] | undefined
+  ELEMENT_DATA: PeriodicElement[] = [
+    {name: 'Oooops',},
+    {name: 'Helium',},
+    {name: 'Lithium',},
+
+  ];
+
+  displayedColumns: string[] = ['position', 'task', 'actions'];
+  dataSource = this.ELEMENT_DATA;
+
+  constructor(
+    private listService: TodoListService
+  ) {
   }
 
   ngOnInit(): void {
+    const gSub = this.listService.getAllLists()
+      .subscribe({
+          next: (response) => {
+            console.log('Get all:', response)
+          }
+        }
+      )
+    this.allSubs?.add(gSub)
   }
 
+  ngOnDestroy(): void {
+    if (this.allSubs) {
+        this.allSubs.unsubscribe()
+    }
+  }
 }
