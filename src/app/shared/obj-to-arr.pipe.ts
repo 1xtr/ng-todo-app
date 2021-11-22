34d475364854d17e7a684d05cf68ea09a797010c
Aleksey Anisimov <xtr@trendco.space>
2021-11-22 08:12:48 +0300

    remove unused types

--- src/app/shared/obj-to-arr.pipe.ts
+++ src/app/shared/obj-to-arr.pipe.ts
@@ -1,7 +1,5 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {FBObjData, ITask, ITodoList} from "./Interfaces";
-
-// type inputDataAlias = ITask | ITodoList
+import {FBObjData, ITodoList} from "./Interfaces";
 
 @Pipe({
   name: 'FBObjToArr'
@@ -26,24 +24,5 @@ export class ObjToArrPipe implements PipeTransform {
   transform(source: FBObjData<ITodoList>): ITodoList[] {
     return this.objToArr<ITodoList>(source)
   }
-
-  // objToArr<T>(source: { keyof T: T }): T[] {
-  //   return Object
-  //     .keys(source)
-  //     .map((key: string): T => ({
-  //           ...source[key],
-  //           id: key
-  //         }
-  //       )
-  //     )
-  // }
-
-  // transform(fbObj: FBObjData<ITodoList> | undefined): ITodoList[] {
-  //   if (fbObj) {
-  //     fbObj['tasks'] = this.objToArr<ITask>(fbObj['tasks']) as ITask[]
-  //     return this.objToArr<ITodoList>(fbObj)
-  //   }
-  //   return []
-  // }
 
 }
