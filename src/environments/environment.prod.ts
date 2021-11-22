8ddf9af616096979508901d16498b52c4abc5be2
Aleksey Anisimov <xtr@trendco.space>
2021-11-22 08:13:36 +0300

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-import { Environment } from "./interface";
+import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
@@ -11,7 +11,7 @@ export const environment: Environment = {
   REFRESH_TOKEN_URL: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
   GET_USER_DATA_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
   UPDATE_USER_DATA_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
-  FB_DB_URL: 'https://test-project-67b13-default-rtdb.europe-west1.firebasedatabase.app',
+  FB_DB_URL: 'https://test-project-67b13-default-rtdb.europe-west1.firebasedatabase.app/angular-todo-app',
   firebaseConfig: {
     apiKey: "AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y",
     authDomain: "test-project-67b13.firebaseapp.com",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import { Environment } from "./interface";
+import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: false,
@@ -15,7 +15,7 @@ export const environment: Environment = {
   REFRESH_TOKEN_URL: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
   GET_USER_DATA_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
   UPDATE_USER_DATA_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y',
-  FB_DB_URL: 'https://test-project-67b13-default-rtdb.europe-west1.firebasedatabase.app',
+  FB_DB_URL: 'https://test-project-67b13-default-rtdb.europe-west1.firebasedatabase.app/angular-todo-app',
   firebaseConfig: {
     apiKey: "AIzaSyDC-HkFhPqCnfbb56wxkgAnfPPkhPAHn-Y",
     authDomain: "test-project-67b13.firebaseapp.com",
