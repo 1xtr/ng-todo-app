4c8ec5bdcf7cd425ef2550e4c2ae80a288e47309
Aleksey Anisimov <xtr@trendco.space>
2021-11-23 22:14:15 +0300

    create todo component

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-export interface Todo {
-  id: string
-  title: string
-  list_id: string
-  deadline_date?: string
-}
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
+import {SnackBarService} from "../_services/snack-bar.service";
+import {TodoListService} from "../_services/todo-list.service";
+import {ITodoList} from "../shared/Interfaces";
+import {AuthService} from "../_services/auth.service";
 
 @Component({
   selector: 'app-todo',
@@ -13,10 +12,38 @@ export interface Todo {
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+  initialState!: ITodoList;
+  sharedTodo!: ITodoList;
+  allSubs: Subscription | undefined
+  displayedColumns: string[] = ['position', 'task', 'actions'];
+  isLoading: boolean = true;
 
-  constructor() { }
+  constructor(
+    private alert: SnackBarService,
+    private route: ActivatedRoute,
+    public auth: AuthService,
+    public listService: TodoListService,
+  ) {
+  }
 
   ngOnInit(): void {
+    const routeSub = this.route.params.subscribe(({fragment}) => {
+      if (fragment) {
+        this.listService.getSharedTodoByFragment(fragment).subscribe({
+          next: ({todoId}) => {
+            this.listService.getTodoById(todoId).subscribe(
+              (response) => {
+                this.sharedTodo = this.initialState = response
+                this.isLoading = false
+              }
+            )
+          },
+          error: () => this.alert.error('Shared todo not found')
+        })
+      }
+    })
+    this.allSubs?.add(routeSub)
+
   }
 
 }
