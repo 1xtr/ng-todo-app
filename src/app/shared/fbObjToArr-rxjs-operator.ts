d95fe03e5d6fc7e4aa2e08dd3149a23de6d79ee2
Aleksey Anisimov <xtr@trendco.space>
2021-12-01 21:47:47 +0300

    comment unused

--- src/app/shared/fbObjToArr-rxjs-operator.ts
+++ src/app/shared/fbObjToArr-rxjs-operator.ts
@@ -1,13 +1,13 @@
-import {map, OperatorFunction} from "rxjs";
-
-export function fbObjToArr(): OperatorFunction<any, any> {
-  return map((response) => {
-    return Object.keys(response)
-      .map((key): any => ({
-            ...response[key],
-            id: key
-          }
-        )
-      )
-  })
-}
+// import {map, OperatorFunction} from "rxjs";
+//
+// export function fbObjToArr(): OperatorFunction<any, any> {
+//   return map((response) => {
+//     return Object.keys(response)
+//       .map((key): any => ({
+//             ...response[key],
+//             id: key
+//           }
+//         )
+//       )
+//   })
+// }
